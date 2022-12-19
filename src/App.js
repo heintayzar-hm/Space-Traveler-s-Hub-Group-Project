0c40b2ea41f87b98450edd88d0c7d0eae6225fb6
Enning94 <enningfestus@gmail.com>
2022-12-19 10:13:06 +0000

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Missions from './Components/Pages/MissionsPage';
+import Rockets from './Components/Pages/RocketsPage';
+import Profile from './Components/Pages/Profile';
 
 function App() {
   return (
-    <p>Hello World</p>
+    <Routes>
+      <Route path="/Rockets" element={<Rockets />} />
+      <Route path="/Missions" element={<Missions />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/*" element={<Rockets />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import store from './Redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
 
