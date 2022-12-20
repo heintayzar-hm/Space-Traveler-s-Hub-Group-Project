8c26dfccb2bd392f4fba1e995dcc8bf4de3ec627
Hein Tay Zar <heintayzarhm@gmail.com>
2022-12-20 16:18:29 +0630

    Update headers and footers

--- src/Components/layout/header.js
+++ src/Components/layout/header.js
@@ -23,18 +23,30 @@ const Header = () => {
   return (
     <>
       <nav className=" grid grid-cols-5 p-3">
-        <h1 className=" col-span-2 m-auto flex justify-center">
-          <NavLink
-            to={links[0].path}
-            data-testid="link-item"
-          >
-            <img src={Logo} alt="logo" />
-          </NavLink>
+        <h1 className=" col-span-2 m-auto flex justify-center w-full">
+
+          <div className="flex w-full justify-around">
+            <NavLink
+              to={links[0].path}
+              data-testid="link-item"
+              className=""
+            >
+              <img src={Logo} alt="logo" />
+            </NavLink>
+            <NavLink
+              to={links[0].path}
+              data-testid="link-item"
+              className=""
+            >
+              <h1 className="md:text-2xl hover:text-blue-600 hidden sm:block text-xl font-bold font-italics">Space Travelers Hub</h1>
+
+            </NavLink>
+          </div>
         </h1>
         <div className="col-span-3 flex justify-center">
           <ul className="grid grid-cols-3 w-full">
             {links.map((link) => (
-              <li key={link.id}>
+              <li key={link.id} className="font-normal text-base hover:text-blue-600">
                 {' '}
                 <NavLink
                   to={link.path}
