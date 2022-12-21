b7e30ce0dc26c1c6f6042dfdc38149327f715a98
Hein Tay Zar <heintayzarhm@gmail.com>
2022-12-21 17:52:00 +0630

    Change style

--- src/Components/missionProfile.js
+++ src/Components/missionProfile.js
@@ -6,11 +6,11 @@ const MissionProfile = () => {
     (mission) => mission.reserved === true,
   );
   return (
-    <div className="mission-profile">
-      <h2>My Missions</h2>
+    <div className="">
+      <h2 className="text-2xl font-bold font-important">My Missions</h2>
       <div>
-        <ul className="mission-profile-ul">
-          {reservedMissions.map((mission) => <li key={mission.id}>{mission.name}</li>)}
+        <ul className="grid grid-cols-1 my-4 mx-2">
+          {reservedMissions.map((mission) => <li className="p-5 text-base font-normal border-brown-200 border" key={mission.id}>{mission.name}</li>)}
         </ul>
       </div>
     </div>
