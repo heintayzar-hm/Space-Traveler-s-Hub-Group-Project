a47a04cb321cca8ee5109f52da40c4d1868a6fc8
Enning94 <enningfestus@gmail.com>
2022-12-21 08:49:21 +0000

    switch badges for missions

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,16 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+// import rocketsReducer from './rockets/RocketReducer';
+import missionReducer from './mission/mission';
+
+const rootReducer = combineReducers({
+//   rocketsReducer,
+  missionReducer,
+});
 
 const store = configureStore({
-  reducer: {
-  },
+  reducer: rootReducer,
 }, applyMiddleware(thunk, logger));
 
 export default store;
