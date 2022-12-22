ad3045046566feed0b19daefe4a99e4eb650e66f
Enning94 <enningfestus@gmail.com>
2022-12-22 14:27:39 +0000

    MissionPage component test

--- src/__test__/Misssions.test.js
+++ src/__test__/Misssions.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import Missions from "../"
+import Missions from '../Components/Pages/MissionsPage';
 import store from '../Redux/configureStore';
 
 describe('Renders correctly', () => {
